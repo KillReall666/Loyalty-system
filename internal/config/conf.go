b34b9760d39f3b132d247b6a491fcfd97a8d8f30
KillReall666 <codewarrior666@mail.ru>
2023-12-01 19:44:20 +0300

    Прододжаю бороться с подключением, внесены изменения в файл config

--- internal/config/conf.go
+++ internal/config/conf.go
@@ -9,11 +9,13 @@ import (
 type RunConfig struct {
 	Address          string `env:"ADDRESS"`
 	DefaultDBConnStr string `env:"DATABASE_DSN"`
+	AccrualAddress   string `env:"ACCRUAL_SYSTEM_ADDRESS"`
 }
 
 const (
 	defaultServer  = "" //":8080"
 	defaultConnStr = "" //"host=localhost user=Mr8 password=Rammstein12! dbname=loyalty_system sslmode=disable"
+	defaultAccrualAddress
 )
 
 func LoadConfig() RunConfig {
@@ -21,6 +23,7 @@ func LoadConfig() RunConfig {
 
 	flag.StringVar(&cfg.Address, "a", defaultServer, "server address [host:port]")
 	flag.StringVar(&cfg.DefaultDBConnStr, "d", defaultConnStr, "connection string")
+	flag.StringVar(&cfg.AccrualAddress, "r", defaultAccrualAddress, "accrual connection string")
 	flag.Parse()
 
 	err := env.Parse(&cfg)
